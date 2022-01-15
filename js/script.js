890004d1ec7e9fe5779393953264cf57ee16eb3e
Levi <walterlevi98@gmail.com>
2022-01-15 17:29:31 -0600

    V1.0

---
+++ js/script.js
@@ -0,0 +1,21 @@
+//collapse hamburger menu after do click
+$('.js-scroll-trigger').click(function() {
+    $('.navbar-collapse').collapse('hide');
+  });
+
+//back-top
+/*$document.ready(function(){
+  $('.my-class-button').click(function(){
+    $('body, html').animate({
+      scrollTop: '0px'
+    }, 1000);
+  });
+  $(window).scroll(function(){
+    if($(this).scrollTop() > 0){
+      $('.my-class-button').slideDown(1000)
+    }
+    else{
+      $('.my-class-button').slideUp(1000)
+    }
+  })
+});*/
